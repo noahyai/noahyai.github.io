5d548b0b4511bfe5e3f9bdc369ec7c002e3aeff0
Samuel Carlisle <samuelcarlisle@gmail.com>
2024-02-14 03:53:26 +0100

    rearranged the content

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ if (document.getElementById('my-work-link')) {
   })
 }
 
+if (document.getElementById('my-project-link')) {
+  document.getElementById('my-project-link').addEventListener('click', () => {
+    document.getElementById('my-project-section').scrollIntoView({behavior: "smooth"})
+  })
+}
+
 if (document.getElementById('my-service-link')) {
   document.getElementById('my-service-link').addEventListener('click', () => {
     document.getElementById('my-service-section').scrollIntoView({behavior: "smooth"})
